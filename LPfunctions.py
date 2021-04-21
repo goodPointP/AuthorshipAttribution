9585685d4decbb49921144ff58bcea5ce4fcfff9
Sllambias <Llambias@live.com>
2021-04-21 13:44:54 +0200

    light function

--- LPfunctions.py
+++ LPfunctions.py
@@ -1,10 +1,17 @@
 ### import.py
 import json
 import pandas as pd
-text = 'data/pan20-authorship-verification-training-small.jsonl'
-def textimport(text):
+def textimport():
     data = []
-    with open(text) as f:
+    with open('data/pan20-authorship-verification-training-small.jsonl') as f:
         for l in f:
+            data.append(json.loads(l.strip()))
+    return pd.DataFrame.from_dict(data)
+
+
+def textimport_light():
+    data = []
+    with open('data/pan20-authorship-verification-training-small.jsonl') as f:
+        for l in f.readlines(100000000):
             data.append(json.loads(l.strip()))
     return pd.DataFrame.from_dict(data)
