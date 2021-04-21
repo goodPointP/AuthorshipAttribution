1e7783aed740d86f88550e5da62eab684155b480
Sllambias <Llambias@live.com>
2021-04-21 14:30:05 +0200

    truth versions

--- LPfunctions.py
+++ LPfunctions.py
@@ -1,6 +1,7 @@
 ### import.py
 import json
 import pandas as pd
+
 def textimport():
     data = []
     with open('data/pan20-authorship-verification-training-small.jsonl') as f:
@@ -8,6 +9,12 @@ def textimport():
             data.append(json.loads(l.strip()))
     return pd.DataFrame.from_dict(data)
 
+def truthimport():
+    data_truth = []
+    with open('pan20-authorship-verification-training-small-truth.jsonl') as f:
+        for l in f:
+            data_truth.append(json.loads(l.strip()))
+    return pd.DataFrame.from_dict(data_truth)
 
 def textimport_light():
     data = []
@@ -15,3 +22,10 @@ def textimport_light():
         for l in f.readlines(100000000):
             data.append(json.loads(l.strip()))
     return pd.DataFrame.from_dict(data)
+
+def truthimport_light():
+    data_truth = []
+    with open('data/pan20-authorship-verification-training-small-truth.jsonl') as f:
+        for l in f:
+            data_truth.append(json.loads(l.strip()))
+    return pd.DataFrame.from_dict(data_truth)[:2289]
