f7fc2664f4f26fb3726eeb842bae2fff060cce9d
goodPointP <49491425+goodPointP@users.noreply.github.com>
2021-04-21 12:00:58 +0200

    Add files via upload

---
+++ import.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 14:53:06 2021
+
+@author: John
+"""
+
+import json
+import pandas as pd
+
+data = []
+with open('data/pan20-authorship-verification-training-small.jsonl') as f:
+    for l in f:
+        data.append(json.loads(l.strip()))
+        
+df = pd.DataFrame.from_dict(data)
+
+#%%
+
+df = df.head(1000)
