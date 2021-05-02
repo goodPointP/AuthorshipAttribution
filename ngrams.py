432e38bb8d5e2a100fcffb295a5a4e30d8ea90c5
Sllambias <Llambias@live.com>
2021-05-02 13:51:21 +0200

    slight changes for efficiency

--- ngrams.py
+++ ngrams.py
@@ -12,36 +12,34 @@ text_uniques = remove_duplicates(dataframe)[1]
 
 #%%
 
-def count_word_ngrams(text, dataset, n):
-    c_w_ngrams = CountVectorizer(ngram_range = (n,n))
+def count_word_ngrams(dataset, min_n, max_n):
+    c_w_ngrams = CountVectorizer(ngram_range = (min_n, max_n))
     c_w_ngrams_vec = c_w_ngrams.fit_transform(dataset).toarray()
-    text_vec = c_w_ngrams_vec[np.where(dataset == text)]
-    
-    return text_vec.T, c_w_ngrams_vec
+ 
+    return c_w_ngrams_vec
 
-def count_char_ngrams(text, dataset, n):
-    c_c_ngrams = CountVectorizer(analyzer = "char", ngram_range = (n,n))
+def count_char_ngrams(dataset, min_n, max_n):
+    c_c_ngrams = CountVectorizer(analyzer = "char", ngram_range = (min_n, max_n))
     c_c_ngrams_vec = c_c_ngrams.fit_transform(dataset).toarray()
-    text_vec = c_c_ngrams_vec[np.where(dataset == text)]
-    
-    return text_vec.T, c_c_ngrams_vec
 
-def tfidf_word_ngrams(text, dataset, n):
-    t_w_ngrams = TfidfVectorizer(ngram_range = (n,n))
+    return c_c_ngrams_vec
+
+def tfidf_word_ngrams(dataset, min_n, max_n):
+    t_w_ngrams = TfidfVectorizer(ngram_range = (min_n, max_n))
     t_w_ngrams_vec = t_w_ngrams.fit_transform(dataset).toarray()
-    text_vec = t_w_ngrams_vec[np.where(dataset == text)]
-    
-    return text_vec.T, t_w_ngrams_vec
 
-def tfidf_char_ngrams(text, dataset, n):
-    t_c_ngrams = TfidfVectorizer(analyzer = "char", ngram_range = (n,n))
+    return t_w_ngrams_vec
+
+def tfidf_char_ngrams(dataset, min_n, max_n):
+    t_c_ngrams = TfidfVectorizer(analyzer = "char", ngram_range = (min_n, max_n))
     t_c_ngrams_vec = t_c_ngrams.fit_transform(dataset).toarray()
-    text_vec = t_c_ngrams_vec[np.where(dataset == text)]
     
-    return text_vec.T, t_c_ngrams_vec
+    return t_c_ngrams_vec
+
+#the code I removed == c_w_ngrams_vec[np.where(dataset == text)]
 
 start = time.time()
-one = count_word_ngrams(text_uniques[0], text_uniques[0:100], 3)
+one = count_word_ngrams(text_uniques[0:100], 1, 3)
 #two = count_char_ngrams(text_uniques[0], text_uniques, 3)
 #three = tfidf_word_ngrams(text_uniques[0], text_uniques, 3)
 #four = tfidf_char_ngrams(text_uniques[0], text_uniques, 3)
