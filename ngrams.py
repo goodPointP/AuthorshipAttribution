7a6b00b9a5c3eabb7569a0a3df545551ed9a3c8b
Sllambias <Llambias@live.com>
2021-05-07 15:11:27 +0200

    new and better and stronger

--- ngrams.py
+++ ngrams.py
@@ -12,26 +12,14 @@ text_uniques = remove_duplicates(dataframe)[1]
 
 #%%
 
-def count_word_ngrams(dataset, min_n, max_n):
-    c_w_ngrams = CountVectorizer(ngram_range = (min_n, max_n))
-    c_w_ngrams_vec = c_w_ngrams.fit_transform(dataset).toarray()
- 
-    return c_w_ngrams_vec
-
-def count_char_ngrams(dataset, min_n, max_n):
-    c_c_ngrams = CountVectorizer(analyzer = "char", ngram_range = (min_n, max_n))
-    c_c_ngrams_vec = c_c_ngrams.fit_transform(dataset).toarray()
-
-    return c_c_ngrams_vec
-
 def tfidf_word_ngrams(dataset, min_n, max_n):
-    t_w_ngrams = TfidfVectorizer(ngram_range = (min_n, max_n))
+    t_w_ngrams = TfidfVectorizer(ngram_range = (min_n, max_n), max_features=200)
     t_w_ngrams_vec = t_w_ngrams.fit_transform(dataset).toarray()
 
     return t_w_ngrams_vec
 
 def tfidf_char_ngrams(dataset, min_n, max_n):
-    t_c_ngrams = TfidfVectorizer(analyzer = "char", ngram_range = (min_n, max_n))
+    t_c_ngrams = TfidfVectorizer(analyzer = "char", ngram_range = (min_n, max_n), max_features=200)
     t_c_ngrams_vec = t_c_ngrams.fit_transform(dataset).toarray()
     
     return t_c_ngrams_vec
@@ -39,10 +27,13 @@ def tfidf_char_ngrams(dataset, min_n, max_n):
 #the code I removed == c_w_ngrams_vec[np.where(dataset == text)]
 
 start = time.time()
-one = count_word_ngrams(text_uniques[0:100], 1, 3)
-#two = count_char_ngrams(text_uniques[0], text_uniques, 3)
-#three = tfidf_word_ngrams(text_uniques[0], text_uniques, 3)
+three = tfidf_word_ngrams(text_uniques , 1, 1)
 #four = tfidf_char_ngrams(text_uniques[0], text_uniques, 3)
 end = time.time()
 
 print(end - start)
+
+#%%
+no = int(len(corpus))
+tagged_pairs = corpus[np.array(list(df['text_id'][:int(no/2)]))]
+cos = [spatial.distance.pdist(pair, metric='cosine') for pair in tagged_pairs]
