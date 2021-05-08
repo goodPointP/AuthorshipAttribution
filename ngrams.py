2b8aea48c1415c615924de7c60f4530092c9b375
ScandinavianMrT <troelspmjensen@gmail.com>
2021-05-08 12:14:41 +0200

    output is now cosine sim

--- ngrams.py
+++ ngrams.py
@@ -1,39 +1,43 @@
 from functions import *
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import time
 
 #%%
 
 raw_data = read_data()
-dataframe = textimport_light(raw_data, pandas_check = True)
-text_uniques = remove_duplicates(dataframe)[1]
-
+df = textimport_light(raw_data, pandas_check = True)
+text_IDs, text_uniques = remove_duplicates(df)
+df['text_id'] = pd.Series(zip(text_IDs[0::2], text_IDs[1::2]))
 
 #%%
 
-def tfidf_word_ngrams(dataset, min_n, max_n):
+def tfidf_word_ngrams(corpus, min_n, max_n):
     t_w_ngrams = TfidfVectorizer(ngram_range = (min_n, max_n), max_features=200)
-    t_w_ngrams_vec = t_w_ngrams.fit_transform(dataset).toarray()
-
-    return t_w_ngrams_vec
-
-def tfidf_char_ngrams(dataset, min_n, max_n):
-    t_c_ngrams = TfidfVectorizer(analyzer = "char", ngram_range = (min_n, max_n), max_features=200)
-    t_c_ngrams_vec = t_c_ngrams.fit_transform(dataset).toarray()
+    t_w_ngrams_vec = t_w_ngrams.fit_transform(corpus).toarray()
     
-    return t_c_ngrams_vec
+    no = int(len(corpus))
+    tagged_pairs = t_w_ngrams_vec[np.array(list(df['text_id'][:int(no/2)]))]
+    cos = [spatial.distance.pdist(pair, metric='cosine') for pair in tagged_pairs]
 
-#the code I removed == c_w_ngrams_vec[np.where(dataset == text)]
+    return np.concatenate(cos, axis = 0)
+
+def tfidf_char_ngrams(corpus, min_n, max_n):
+    t_c_ngrams = TfidfVectorizer(analyzer = "char", ngram_range = (min_n, max_n), max_features=200)
+    t_c_ngrams_vec = t_c_ngrams.fit_transform(corpus).toarray()
+    
+    no = int(len(corpus))
+    tagged_pairs = t_c_ngrams_vec[np.array(list(df['text_id'][:int(no/2)]))]
+    cos = [spatial.distance.pdist(pair, metric='cosine') for pair in tagged_pairs]
+
+    return np.concatenate(cos, axis = 0)
+
 
 start = time.time()
-three = tfidf_word_ngrams(text_uniques , 1, 1)
-#four = tfidf_char_ngrams(text_uniques[0], text_uniques, 3)
+#three = tfidf_word_ngrams(text_uniques, 1, 1) # 111s
+four = tfidf_char_ngrams(text_uniques[0:100], 3, 3)
 end = time.time()
 
 print(end - start)
 
 #%%
-no = int(len(corpus))
-tagged_pairs = corpus[np.array(list(df['text_id'][:int(no/2)]))]
-cos = [spatial.distance.pdist(pair, metric='cosine') for pair in tagged_pairs]
